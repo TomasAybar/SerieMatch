0b625ab73ee304e4ff90f52ed1fc9ea6e73e7030
Mateo <mateoluquetti@gmail.com>
2026-09-01 19:59:21 -0300

    TP0: Agregada la carpeta modelos con usuario y serie

---
+++ modelos/serie.py
@@ -0,0 +1,5 @@
+class Serie:
+    def __init__(self, titulo, genero, rating):
+        self._titulo = titulo
+        self._genero = genero
+        self._rating = rating
---
+++ modelos/usuario.py
@@ -0,0 +1,5 @@
+class Usuario:
+    def __init__(self, nombre, edad):
+        self._nombre = nombre
+        self._edad = edad
+        self._generos_favoritos = []
