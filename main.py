7779240a4439ec493d3ba5f0b575d83eb3f94148
Mateo <mateoluquetti@gmail.com>
2026-09-16 16:21:17 -0300

    TP3: Crea instancia de ArbolBST e inserta las series al mismo en main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from modelos.catalogo import Catalogo
 from ui.consola import ConsolaUI
+from estructuras.arbol_binario import ArbolBST
 
 
 def main():
-
     mi_catalogo = Catalogo()
     mi_catalogo.cargar_datos(ruta_archivo="datos/series.json")
 
-    app = ConsolaUI(catalogo=mi_catalogo)
+    arbol = ArbolBST()
+    for serie in mi_catalogo.series:
+        arbol.insertar(serie, clave=lambda s: s.titulo.lower())
 
-    # Inicio app
+    app = ConsolaUI(catalogo=mi_catalogo, arbol=arbol)
     app.iniciar_aplicacion()
 
 
 if __name__ == "__main__":
-    main()
+    main()  
